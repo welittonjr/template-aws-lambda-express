e52cd9ae1d9b83abd5fb2af28d241fe50fd7c1fc
Wellington <wellington.junior@madeiramadeira.com.br>
2021-09-30 15:22:28 -0300

    ajustes finais

--- app.ts
+++ app.ts
@@ -1,3 +1,7 @@
-export const handler = async () => {
-  return 'Hello';
-};
+import { createServer, proxy } from 'aws-serverless-express';
+import boot from './src/boot';
+const server = createServer(boot);
+
+export const handler = (event: any, context: any) => {
+  proxy(server, event, context);
+}
--- src/boot.ts
+++ src/boot.ts
@@ -1,8 +1,23 @@
 import express from "express";
-const server = express();
+const app = express();
+const router = express.Router()
 
-server.get("/", (request, response) => {
-  return response.send("Hello!");
+router.get("/", (_, res) => {
+  res.status(200).json({ status: 200 })
 });
 
-export default server;
+router.get("/alive", (_, res) => {
+  res.status(200).json({ message: "I\'m alive!" })
+});
+
+router.get("/docs", (_, res) => {
+  res.status(200).json({ message: "docs" })
+});
+
+router.get("*", (_, res) => {
+  res.status(404).json({ message: "Page Not Found!" })
+});
+
+app.use(router)
+
+export default app;

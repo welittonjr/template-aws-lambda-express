1d7ed69e6c8264b0e4d28b84f48a277ff161ceaf
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-11-25 14:41:52 -0300

    iniciando rotas

--- src/boot.ts
+++ src/boot.ts
@@ -1,23 +1,8 @@
 import express from "express";
+import router from "./routes";
 const app = express();
-const router = express.Router()
 
-router.get("/", (_, res) => {
-  res.status(200).json({ status: 200 })
-});
-
-router.get("/alive", (_, res) => {
-  res.status(200).json({ message: "I\'m alive!" })
-});
-
-router.get("/docs", (_, res) => {
-  res.status(200).json({ message: "docs" })
-});
-
-router.get("*", (_, res) => {
-  res.status(404).json({ message: "Page Not Found!" })
-});
-
+app.use(express.json())
 app.use(router)
 
 export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+const router = express.Router()
+
+router.get("/", (_, res) => {
+  res.status(200).json({ status: 200 })
+});
+
+router.get("/alive", (_, res) => {
+  res.status(200).json({ message: "I\'m alive!" })
+});
+
+router.get("/docs", (_, res) => {
+  res.status(200).json({ message: "docs" })
+});
+
+router.get("*", (_, res) => {
+  res.status(404).json({ message: "Page Not Found!" })
+});
+
+export default router;

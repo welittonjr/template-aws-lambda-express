2bad062283b9a79910ebe7d7b1e2671ce06ea93e
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-11-29 09:09:23 -0300

    script para criar fila no localstack

---
+++ lambda_sqs/scripts/sqs/create-queue.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# Script for creation queue
+
+if [ -z "$1" ]; then
+  echo 'Queue name must be informed'
+  exit 1
+else
+  aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name $1
+fi

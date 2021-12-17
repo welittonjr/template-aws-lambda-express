de2d3224b0ba8d6ad125ebb5484a923088680024
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-12-17 16:13:26 -0300

    atualizaçao em scripts localstack sqs

--- lambda_sqs/scripts/sqs/create-queue.sh
+++ lambda_sqs/scripts/sqs/create-queue.sh
@@ -5,5 +5,5 @@ if [ -z "$1" ]; then
   echo 'Queue name must be informed'
   exit 1
 else
-  aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name $1
+  aws --endpoint-url=http://localhost:4567 sqs create-queue --queue-name $1
 fi
--- lambda_sqs/scripts/sqs/list-queues.sh
+++ lambda_sqs/scripts/sqs/list-queues.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 # Script for list all queues created
 
-aws --endpoint-url=http://localhost:4566 sqs list-queues
+aws --endpoint-url=http://localhost:4567 sqs list-queues
+
+if [ $? -eq 0 ]; then
+  echo "There is no queue"
+fi
